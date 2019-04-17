0130198224995a30450c39bebb2bffcd5baf0afc
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-04-17 15:56:51 +0200

    Refactor with defaults values to minimize side configurations

--- kernel-updater.sh
+++ kernel-updater.sh
@@ -7,6 +7,20 @@ set -o pipefail
 [ "$DEBUG" = "1" ] && set -x
 
 LOCALDIR="$(readlink -f "$(dirname "$0")")"
+[[ "x$BUILDDIR" == "x" ]] && BUILDDIR="$LOCALDIR"
+
+BUILDERDIR="$BUILDDIR/qubes-builder-kernel-${BRANCH_linux_kernel}"
+
+GIT_UPSTREAM='QubesOS'
+GIT_FORK='fepitre-bot'
+
+GIT_BASEURL_UPSTREAM="https://github.com"
+GIT_PREFIX_UPSTREAM="$GIT_UPSTREAM/qubes-"
+
+GIT_BASEURL_FORK="git@github.com:"
+GIT_PREFIX_FORK="$GIT_FORK/qubes-"
+
+GITHUB_TOKEN_FILE="$HOME/.github_api_token"
 
 if [ -e "$UPDATER_CONF" ]; then
     # shellcheck source=/dev/null
@@ -27,8 +41,8 @@ done
 exit_launcher() {
     local exit_code=$?
 
-    pushd "$LOCALDIR/qubes-builder"
-    make distclean
+    pushd "$BUILDERDIR"
+    make clean-chroot
     popd
 
     if [ ${exit_code} -ge 1 ]; then
@@ -47,63 +61,60 @@ distance_version() {
 
 LATEST_KERNEL_VERSION="$("$LOCALDIR/kernel-updater.py" --token "$GITHUB_TOKEN_FILE" --check-update --base "$GIT_UPSTREAM:${BRANCH_linux_kernel:-master}")"
 
-if [ "x$LATEST_KERNEL_VERSION" != "x" ]; then
-    if [ -e "$LOCALDIR/qubes-builder" ]; then
-        "$LOCALDIR/umount_kill.sh" "$LOCALDIR/qubes-builder"
-        sudo rm -rf "$LOCALDIR/qubes-builder"
-    fi
-    git clone "${GIT_BASEURL_UPSTREAM}/${GIT_PREFIX_UPSTREAM}builder" "$LOCALDIR/qubes-builder"
-    cp "$LOCALDIR/qubes-builder.conf" "$LOCALDIR/qubes-builder/builder.conf"
-
-    pushd "$LOCALDIR/qubes-builder"
-    make get-sources-git GIT_BASEURL="${GIT_BASEURL_UPSTREAM}" GIT_PREFIX="${GIT_PREFIX_UPSTREAM}"
-
-    pushd "qubes-src/linux-kernel"
-    HEAD_BRANCH="update-v$LATEST_KERNEL_VERSION"
-    git checkout -b "$HEAD_BRANCH"
-    echo "$LATEST_KERNEL_VERSION" > version
-    echo 1 > rel
-    make get-sources
-
-    if [ "$BRANCH_linux_kernel" == "master" ]; then
-        FC_LATEST="$(curl -s -L https://dl.fedoraproject.org/pub/fedora/linux/releases | sed -e 's/<[^>]*>//g' | awk '{print $1}' | grep -o "[0-9][1-9]" | tail -1)"
-        FC_STABLE="$(dnf -q repoquery kernel --disablerepo=*modular* --releasever="$FC_LATEST" | tail -1 | cut -d ':' -f2 | cut -d '-' -f1)"
-        FC_RAWHIDE="$(dnf -q repoquery kernel --disablerepo=*modular* --releasever=rawhide | tail -1 | cut -d ':' -f2 | cut -d '-' -f1)"
-
-        if distance_version "$FC_STABLE" "$LATEST_KERNEL_VERSION"; then
-            ./get-fedora-latest-config "$FC_LATEST"
-            mv config-base-"$FC_STABLE" config-base
-        elif distance_version "$FC_RAWHIDE" "$LATEST_KERNEL_VERSION"; then
-            ./get-fedora-latest-config rawhide
-            mv config-base-"$FC_RAWHIDE" config-base
-        else
-            echo "-> Cannot determine latest config for kernel ${LATEST_KERNEL_VERSION}. Use the current existing config..."
-        fi
-    fi
-    popd
-
-    trap 'exit_launcher' ERR EXIT INT TERM
-
-    make linux-kernel USE_QUBES_REPO_VERSION="$RELEASE" USE_QUBES_REPO_TESTING=1
-
-    pushd "qubes-src/linux-kernel"
-    git add version rel config-base
-    git commit -m "Update to kernel-$LATEST_KERNEL_VERSION"
-    git remote add fork "${GIT_BASEURL_FORK}${GIT_PREFIX_FORK}linux-kernel"
-    git push -u fork "$HEAD_BRANCH"
-    popd
-
-    if [ "x$SIGN_KEY" != "x" ]; then
-       make sign-all COMPONENTS=linux-kernel SIGN_KEY="$SIGN_KEY"
-    fi
-
-    if [ "x$HOST" != "x" ] && [ "x$HOST_BASEDIR" != "x" ]; then
-        make update-repo-unstable COMPONENTS=linux-kernel HOST="$HOST" HOST_BASEDIR="$HOST_BASEDIR"
-    fi
-
-    "$LOCALDIR/kernel-updater.py" --token "$GITHUB_TOKEN_FILE" --create-pullrequest --base "$GIT_UPSTREAM:${BRANCH_linux_kernel:-master}" --head "$GIT_FORK:$HEAD_BRANCH"
-else
+if [ "x$LATEST_KERNEL_VERSION" == "x" ]; then
     echo "-> Current kernel version in branch ${BRANCH_linux_kernel} is up to date"
+    exit 0
 fi
 
+if [ -e "$BUILDERDIR" ]; then
+    "$LOCALDIR/umount_kill.sh" "$BUILDERDIR"
+    sudo rm -rf "$BUILDERDIR"
+fi
 
+trap 'exit_launcher' 0 1 2 3 6 15
+
+git clone "${GIT_BASEURL_UPSTREAM}/${GIT_PREFIX_UPSTREAM}builder" "$BUILDERDIR"
+if [ "x$BUILDERCONF" == "x" ];  then
+    cp "$LOCALDIR/qubes-builder.conf" "$BUILDERDIR/builder.conf"
+fi
+
+pushd "$BUILDERDIR"
+make get-sources-git GIT_BASEURL="${GIT_BASEURL_UPSTREAM}" GIT_PREFIX="${GIT_PREFIX_UPSTREAM}"
+
+pushd "$BUILDERDIR/qubes-src/linux-kernel"
+HEAD_BRANCH="update-v$LATEST_KERNEL_VERSION"
+git checkout -b "$HEAD_BRANCH"
+echo "$LATEST_KERNEL_VERSION" > version
+echo 1 > rel
+make get-sources
+
+if [ "$BRANCH_linux_kernel" == "master" ]; then
+    FC_LATEST="$(curl -s -L https://dl.fedoraproject.org/pub/fedora/linux/releases | sed -e 's/<[^>]*>//g' | awk '{print $1}' | grep -o "[0-9][1-9]" | tail -1)"
+    FC_STABLE="$(dnf -q repoquery kernel --disablerepo=*modular* --releasever="$FC_LATEST" | tail -1 | cut -d ':' -f2 | cut -d '-' -f1)"
+    FC_RAWHIDE="$(dnf -q repoquery kernel --disablerepo=*modular* --releasever=rawhide | tail -1 | cut -d ':' -f2 | cut -d '-' -f1)"
+
+    if distance_version "$FC_STABLE" "$LATEST_KERNEL_VERSION"; then
+        ./get-fedora-latest-config "$FC_LATEST"
+        mv config-base-"$FC_STABLE" config-base
+    elif distance_version "$FC_RAWHIDE" "$LATEST_KERNEL_VERSION"; then
+        ./get-fedora-latest-config rawhide
+        mv config-base-"$FC_RAWHIDE" config-base
+    else
+        echo "-> Cannot determine latest config for kernel ${LATEST_KERNEL_VERSION}. Use the current existing config..."
+    fi
+fi
+popd
+
+make linux-kernel USE_QUBES_REPO_VERSION="$RELEASE" USE_QUBES_REPO_TESTING=1
+
+pushd "$BUILDERDIR/qubes-src/linux-kernel"
+git add version rel config-base
+git commit -m "Update to kernel-$LATEST_KERNEL_VERSION"
+git remote add fork "${GIT_BASEURL_FORK}${GIT_PREFIX_FORK}linux-kernel"
+git push -u fork "$HEAD_BRANCH"
+popd
+
+"$LOCALDIR/kernel-updater.py" --token "$GITHUB_TOKEN_FILE" --create-pullrequest --base "$GIT_UPSTREAM:${BRANCH_linux_kernel:-master}" --head "$GIT_FORK:$HEAD_BRANCH"
+
+# Sign kernel RPMs and update to repository
+make sign-all update-repo-unstable
