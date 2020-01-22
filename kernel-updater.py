e37e6b7497be6442de6c03714273bd6d3cc13821
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-01-22 10:35:52 +0100

    Fix version checking

--- kernel-updater.py
+++ kernel-updater.py
@@ -7,6 +7,7 @@ import json
 
 from github import Github
 from packaging import version
+from packaging.version import parse as parse_version
 
 
 class KernelUpdaterClient(Github):
@@ -25,20 +26,19 @@ class KernelUpdaterClient(Github):
         url_releases = 'https://www.kernel.org/releases.json'
         r = requests.get(url_releases)
         latest_upstream = None
-
         if 200 <= r.status_code < 300:
             content = json.loads(r.content.decode('utf-8'))
-            if self.branch == 'master':
-                releases = [rel['version'] for rel in content['releases'] if rel['moniker'] == 'stable']
-            elif 'stable-' in self.branch:
-                branch_version = self.branch.split('-')[1]
-                releases = [rel['version'] for rel in content['releases'] if
-                            rel['moniker'] == 'longterm' and rel['version'].startswith(branch_version)]
-            else:
-                print('Unknown tag/branch: %s' % self.branch)
+            releases = [rel['version'] for rel in content['releases'] if
+                        rel['moniker'] in ('stable', 'longterm')]
 
-            if releases:
-                latest_upstream = releases[0]
+            releases.sort(key=parse_version, reverse=True)
+
+            if 'stable-' in self.branch:
+                branch_version = self.branch.split('-')[1]
+                releases = [rel for rel in releases if
+                            rel.startswith(branch_version)]
+
+            latest_upstream = releases[0]
         else:
             print('An error occurred while downloading "%s"' % url_releases)
 
@@ -55,9 +55,10 @@ class KernelUpdaterClient(Github):
     def is_update_needed(self):
         version_qubes = self.get_version_qubes()
         version_upstream = self.get_version_upstream()
-        if (not self.is_autopr_present(version_upstream)) and (
-                version.parse(version_qubes) < version.parse(version_upstream)):
-            return version_upstream
+        if version_qubes and version_upstream:
+            if (not self.is_autopr_present(version_upstream)) and (
+                    version.parse(version_qubes) < version.parse(version_upstream)):
+                return version_upstream
 
     def create_pullrequest(self, base, head):
         # example of head: 'fepitre:v4.19.30'
@@ -65,7 +66,8 @@ class KernelUpdaterClient(Github):
         if len(parsed_head) == 2:
             version = parsed_head[1].lstrip('update-v')
         else:
-            print('An error occurred while parsing "repo:branch" from %s' % head)
+            print(
+                'An error occurred while parsing "repo:branch" from %s' % head)
             sys.exit(1)
 
         self.repo.create_pull(title="UPDATE: " + version,
@@ -79,7 +81,8 @@ def parse_args(argv):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--check-update', required=False, action='store_true')
-    parser.add_argument('--create-pullrequest', required=False, action='store_true')
+    parser.add_argument('--create-pullrequest', required=False,
+                        action='store_true')
     parser.add_argument('--token', required=False)
     parser.add_argument('--base', required=True)
     parser.add_argument('--head', required=False)
@@ -99,7 +102,8 @@ def main(argv):
             with open(args.token, 'r') as f:
                 token = f.read().replace('\n', '')
         except IOError:
-            print("An error occurred while reading token file '%s'" % args.token)
+            print(
+                "An error occurred while reading token file '%s'" % args.token)
 
     # example of args.base: 'fepitre:stable-4.19'
     parsed_base = args.base.split(':')
@@ -107,7 +111,8 @@ def main(argv):
         account = parsed_base[0]
         branch = parsed_base[1]
     else:
-        print('An error occurred while parsing "repo:branch" from %s' % args.base)
+        print(
+            'An error occurred while parsing "repo:branch" from %s' % args.base)
         sys.exit(1)
 
     client = KernelUpdaterClient(account=account, branch=branch, token=token)
