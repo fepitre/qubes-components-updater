3d820dc0fcbb7feed10c08e830620fa2d955b515
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-03-25 12:16:14 +0100

    Use travis-iso-full instead for testing repos

--- build-iso.sh
+++ build-iso.sh
@@ -25,6 +25,7 @@ trap 'exit_launcher' 0 1 2 3 6 15
 git clone https://github.com/QubesOS/qubes-builder "$BUILDERDIR"
 make -C "$BUILDERDIR" get-sources BUILDERCONF= COMPONENTS="release-configs" GIT_URL_release_configs=https://github.com/qubesos/qubes-release-configs
 cp "$BUILDERDIR"/qubes-src/release-configs/R4.1/qubes-os-iso-full-online.conf "$BUILDERDIR"/builder.conf
+sed -i "s|iso-full-online.ks|travis-iso-full.ks|" "$BUILDERDIR"/builder.conf
 make -C "$BUILDERDIR" get-sources
 #sed -i 's#\(<packagereq type="\)optional\(">kernel-latest.*$\)#\1mandatory\2#' "$BUILDERDIR"/qubes-src/installer-qubes-os/conf/comps-qubes.xml
 make -C "$BUILDERDIR" install-deps remount iso sign-iso upload-iso
