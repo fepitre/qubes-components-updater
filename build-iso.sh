b840d6c83c7a14332032984bd42861949b7b4de1
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-12-03 16:07:41 +0100

    Split upload from build for ISOs

--- build-iso.sh
+++ build-iso.sh
@@ -4,25 +4,29 @@ set -e
 set -o pipefail
 
 LOCALDIR="$(readlink -f "$(dirname "$0")")"
-TMPDIR="$(mktemp -d -p /home/user)"
-BUILDERDIR="$TMPDIR/qubes-builder"
-
 RELEASE="${1:-4.1}"
+BUILDERDIR="/home/user/iso/builder-${RELEASE}"
+if [ -n "${ISO_FLAVOR}" ]; then
+    BUILDERDIR="${BUILDERDIR}-${ISO_FLAVOR}"
+fi
 
 # Hide sensitive info
 [ "$DEBUG" = "1" ] && set -x
 
 exit_launcher() {
     local exit_code=$?
-    make -C "$BUILDERDIR" distclean || true
-    sudo rm -rf "$TMPDIR"
     if [ ${exit_code} -ge 1 ]; then
-        echo "-> An error occurred during build. Manual update is required"
+        echo "-> An error occurred during build. Manual update is required."
     fi
     exit "${exit_code}"
 }
 
 trap 'exit_launcher' 0 1 2 3 6 15
+
+if [ -d "${BUILDERDIR}" ]; then
+    make -C "$BUILDERDIR" distclean || true
+    sudo rm -rf "$BUILDERDIR"
+fi
 
 git clone https://github.com/QubesOS/qubes-builder "$BUILDERDIR"
 make -C "$BUILDERDIR" get-sources BUILDERCONF= COMPONENTS="release-configs" GIT_URL_release_configs=https://github.com/qubesos/qubes-release-configs
@@ -30,21 +34,4 @@ cp "$BUILDERDIR/qubes-src/release-configs/R${RELEASE}/qubes-os-iso-full-online.c
 sed -i "s|iso-full-online.ks|travis-iso-full.ks|" "$BUILDERDIR"/builder.conf
 echo "USE_QUBES_REPO_TESTING=1" >> "$BUILDERDIR"/builder.conf
 make -C "$BUILDERDIR" get-sources
-make -C "$BUILDERDIR" install-deps remount iso sign-iso upload-iso VERBOSE=0
-
-# We must not have more than one ISO but prevent any issue.
-ISO="$(find "$BUILDERDIR"/iso -name "*.iso" | head -1)"
-ISO_NAME="$(basename "$ISO")"
-ISO_BASE="${ISO_NAME%%.iso}"
-ISO_DATE="$(cat "$BUILDERDIR"/qubes-src/installer-qubes-os/build/ISO/qubes-x86_64/iso/build_latest)"
-
-# Upload log
-rsync "$BUILDERDIR"/build-logs/installer-qubes-os-iso-fc32.log "$HOST:$HOST_ISO_BASEDIR/$ISO_BASE$ISO_SUFFIX.log"
-
-# Trigger openQA test
-BUILD="$ISO_DATE"
-if [ -n "$ISO_FLAVOR" ]; then
-    BUILD="${BUILD}-${ISO_FLAVOR}"
-fi
-BUILD="${BUILD}-${RELEASE}"
-python3 "$LOCALDIR/openqa-trigger-iso-test.py" "${RELEASE}" "${BUILD}" "${ISO_NAME}"
+make -C "$BUILDERDIR" install-deps remount iso sign-iso VERBOSE=0
---
+++ upload-iso.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+set -e
+set -o pipefail
+
+LOCALDIR="$(readlink -f "$(dirname "$0")")"
+RELEASE="${1:-4.1}"
+BUILDERDIR="/home/user/iso/builder-${RELEASE}"
+if [ -n "${ISO_FLAVOR}" ]; then
+    BUILDERDIR="${BUILDERDIR}-${ISO_FLAVOR}"
+fi
+
+# Hide sensitive info
+[ "$DEBUG" = "1" ] && set -x
+
+exit_launcher() {
+    local exit_code=$?
+    if [ ${exit_code} -eq 0 ]; then
+        make -C "$BUILDERDIR" distclean || true
+    elif [ ${exit_code} -ge 1 ]; then
+        echo "-> An error occurred during build. Manual update is required."
+    fi
+    exit "${exit_code}"
+}
+
+trap 'exit_launcher' 0 1 2 3 6 15
+
+if ! [ -e "${BUILDERDIR}" ]; then
+    echo "-> Cannot find artifacts."
+fi
+
+make -C "$BUILDERDIR" upload-iso
+
+# We must not have more than one ISO but prevent any issue.
+ISO="$(find "$BUILDERDIR"/iso -name "*.iso" | head -1)"
+ISO_NAME="$(basename "$ISO")"
+ISO_BASE="${ISO_NAME%%.iso}"
+ISO_DATE="$(cat "$BUILDERDIR"/qubes-src/installer-qubes-os/build/ISO/qubes-x86_64/iso/build_latest)"
+
+# Upload log
+rsync "$BUILDERDIR"/build-logs/installer-qubes-os-iso-fc32.log "$HOST:$HOST_ISO_BASEDIR/$ISO_BASE$ISO_SUFFIX.log"
+
+# Trigger openQA test
+BUILD="$ISO_DATE"
+if [ -n "$ISO_FLAVOR" ]; then
+    BUILD="${BUILD}-${ISO_FLAVOR}"
+fi
+BUILD="${BUILD}-${RELEASE}"
+python3 "$LOCALDIR/openqa-trigger-iso-test.py" "${RELEASE}" "${BUILD}" "${ISO_NAME}"
