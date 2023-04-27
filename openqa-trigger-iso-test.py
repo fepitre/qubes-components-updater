56aa6f95d5c6b593fe2965d351eac6b275aed739
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2023-04-27 18:38:23 +0200

    Use iso-testing

--- openqa-trigger-iso-test.py
+++ openqa-trigger-iso-test.py
@@ -11,6 +11,6 @@ params = {
     'FLAVOR': 'install-iso',
     'ARCH': 'x86_64',
     'BUILD': sys.argv[2],
-    'ISO_URL': f'https://qubes.notset.fr/iso/{sys.argv[3]}',
+    'ISO_URL': f'https://qubes.notset.fr/iso-testing/{sys.argv[3]}',
 }
 print(client.openqa_request('POST', 'isos', params))
